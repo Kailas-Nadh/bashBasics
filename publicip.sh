db7dfeae05dd0528e14f0291263834c6061256a5
Kailas Nadh J <71600908+Kailas-Nadh@users.noreply.github.com>
2023-02-04 12:17:58 +0530

    Update publicip.sh

--- publicip.sh
+++ publicip.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+
+# Add desired location at /desired/location/file.txt
 curl ifconfig.me >> /desired/location/file.txt
 echo >> /desired/location/file.txt
 date >> /desired/location/file.txt
