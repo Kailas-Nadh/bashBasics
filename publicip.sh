a248a511395f24f219279c6a95722dc324844444
Kailas Nadh J <71600908+Kailas-Nadh@users.noreply.github.com>
2023-02-04 12:13:58 +0530

    Add files via upload

---
+++ publicip.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+curl ifconfig.me >> /home/kailas/ifconfig_me.txt
+echo >> /home/kailas/ifconfig_me.txt
+date >> /home/kailas/ifconfig_me.txt
+echo >> /home/kailas/ifconfig_me.txt
+echo >> /home/kailas/ifconfig_me.txt
