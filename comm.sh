104f346a0c860761fcecb1d6f1bbf37862e855a1
Kailas Nadh J <71600908+Kailas-Nadh@users.noreply.github.com>
2023-02-25 17:11:02 +0530

    Add files via upload

---
+++ comm.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#We can use this to comment on bash
+
+echo "Enter your name:"
+read name
+echo ".........."
+echo
+echo
+
+echo "Hello Mr $name"
---
+++ comm_multi.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+<<COMMENTS
+Why is this line even written..?
+COMMENTS
+
+echo "Arey wah"
+echo "Tell yo name buddy:"
+read name
+echo "So yo name is $name"
