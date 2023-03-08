659dd0c6b5e28fe4cc204370148dedd3dc020b2d
Kailas Nadh J <71600908+Kailas-Nadh@users.noreply.github.com>
2023-03-08 22:23:07 +0530

    Add files via upload

---
+++ temp.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+red='\033[0;31m'
+white='\033[0;37m'
+yellow='\033[0;33m'
+while true
+do
+	#vcgencmd measure_temp
+	date
+	temp=$(vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
+	sleep 1
+	echo $temp
+	if [[ $temp > 70 ]] 
+	then
+		echo -e "${red}CRITICAL TEMPERATURE..!!! Temperature above 70'C${white}"
+	elif [[ $temp > 60 ]]
+	then
+		echo -e "${yellow}Temperature above 60'C${white}"
+	fi 
+done
