035ed39f305bbe6c4d686e200246fe32c38e3587
Kailas Nadh J <71600908+Kailas-Nadh@users.noreply.github.com>
2024-08-21 10:24:01 +0530

    OS Alert added

--- temp_rpi.sh
+++ temp_rpi.sh
@@ -1,20 +1,29 @@
-#! /bin/bash
-#Program to monitor rpi temperature
+#!/bin/bash
+# Program to monitor RPi temperature
+
 red='\033[0;31m'
 white='\033[0;37m'
 yellow='\033[0;33m'
+
 while true
 do
-	#vcgencmd measure_temp
-	#date
-	temp=$(vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
-	sleep 1
-	echo $temp
-	if [[ $temp > 70 ]] 
-	then
-		echo -e "${red}CRITICAL TEMPERATURE..!!! Temperature above 70'C${white}"
-	elif [[ $temp > 60 ]]
-	then
-		echo -e "${yellow}Temperature above 60'C${white}"
-	fi 
+    # Get the temperature
+    temp=$(vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
+
+    # Compare the temperature and take action
+    if (( $(echo "$temp > 70") )); then
+        echo -e "${red}CRITICAL TEMPERATURE..!!! Temperature above 70°C${white}"
+        # Send a system-level warning
+        # Using notify-send for graphical desktop notifications (if available)
+        if command -v notify-send &> /dev/null; then
+            notify-send "Temperature Alert" "CRITICAL TEMPERATURE: Temperature above 70°C"
+        fi
+        # Log the warning to system log
+        logger "CRITICAL TEMPERATURE: Temperature above 70°C"
+    elif (( $(echo "$temp > 60"))); then
+        echo -e "${yellow}Temperature above 60°C${white}"
+    fi 
+
+    # Wait for 1 second before checking again
+    sleep 1
 done
